556f9cbabca7d10e754ac6c7f1912beb4f78a24f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-17 23:04:41 +0100

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
     <div data-theme="lemonade" id="app">
-        <!-- the header -->
-        <header class="flex p-10">
-            <span class="flex-1">
-                <h1 class="text-center text-4xl">CURRENCY CONVERTER</h1>
-            </span>
-            <span class="flex-none">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded-full"
-                        @click="toggleTheme">Toggle Theme</button>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded-full"
-                        @click="toggleCurrency">Toggle Currency</button>
-            </span>
-        </header>
+        <Navbar />
     </div>
 </template>
 
-<style scoped>
+<script>
+    import Navbar from './components/navbar.vue'
+    export default {
+        name: "App",
+        components: {
+            Navbar
+        },
+        data() {
+            return {
+                // ...
+            }
+        },
+        methods: {
+            // ...
+        }
+    }
+</script>
 
+<style scoped>
+#app {
+    font-family: 'Nunito', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100vh;
+}
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="navbar bg-base-100 p-5">
+        <div class="navbar-start">
+            <!-- ... -->
+        </div>
+        <div class="navbar-center">
+            <a class="font-bold text-4xl">Currency converter</a>
+        </div>
+        <div class="navbar-end">
+            <div class="nav-item mx-0 mx-lg-1 theme-toggle-btn">
+                <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#theme-modal">
+                    <i class="fas fa-palette"></i>
+                </button>
+            </div>
+            <button class="btn btn-ghost btn-circle">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                </svg>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data() {
+        return {
+            // ...
+        }
+    },
+    methods: {
+        // ...
+    }
+}
+</script>
