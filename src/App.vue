36e3aff72b8af5bbb1dfae370d113be7bb80142a
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-17 21:50:49 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-    <button class="btn btn-primary">Button</button>
+    <div data-theme="lemonade" id="app">
+        <!-- the header -->
+        <header class="flex p-10">
+            <span class="flex-1">
+                <h1 class="text-center text-4xl">CURRENCY CONVERTER</h1>
+            </span>
+            <span class="flex-none">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded-full"
+                        @click="toggleTheme">Toggle Theme</button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded-full"
+                        @click="toggleCurrency">Toggle Currency</button>
+            </span>
+        </header>
+    </div>
 </template>
 
 <style scoped>
