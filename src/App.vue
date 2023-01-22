fcaad15045e7cc10545e7e64e1722e37c06d4cf2
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-22 23:23:47 +0100

    remove currency

--- src/App.vue
+++ src/App.vue
@@ -88,6 +88,13 @@
                            class="flex flex-row items-center justify-center cursor-pointer bg-base-300 p-4 gap-5">
                         <div class="mr-2 grow">
                             <div class="flex flex-row items-center">
+                                <span class="text-2xl font-bold"
+                                      v-if="activeCurrencies.length > 1"
+                                      @click.stop="removeActiveCurrency(index)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
                                 <span class="flex-1 font-bold text-4xl text-primary">{{ currency.currency_code }}</span>
                                 <span class="flex-none">
                                     <svg width="12px" height="12px"
