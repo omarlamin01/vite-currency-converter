d877ef27336705e56c48468fe773d48c0a7d11ae
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-25 00:24:29 +0100

    import vue-i18n

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
+import strings from './assets/strings.json';
+
+const i18n = createI18n({
+    locale: 'en',
+    messages: strings
+})
 
 createApp(App).mount('#app')
+
+App.use(i18n);
