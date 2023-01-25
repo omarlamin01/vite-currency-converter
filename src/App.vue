b6e24f9f2b55ca8fecc14350e16ba74b64905efe
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-25 16:04:00 +0100

    add change lang drop-down

--- src/App.vue
+++ src/App.vue
@@ -75,55 +75,10 @@
                     <div
                         class="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px mt-16 w-56 overflow-y-auto shadow-2xl">
                         <ul class="menu menu-compact gap-1 p-3" tabindex="0">
-                            <li>
-                                <button class="flex active"><img loading="lazy" width="20" height="20" alt="English"
+                            <li v-for="lang in languages" @click="updateLocale(lang.code)">
+                                <button :class="locale === lang.code? 'flex active' : 'flex'"><img loading="lazy" width="20" height="20" :alt="$t(`languages.${lang.code}`)"
                                                                  src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1ec-1f1e7.svg">
-                                    <span class="flex flex-1 justify-between">English </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="Español"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1ea-1f1f8.svg">
-                                    <span class="flex flex-1 justify-between">Español </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="Français"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1eb-1f1f7.svg">
-                                    <span class="flex flex-1 justify-between">Français </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="Indonesia"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1ee-1f1e9.svg">
-                                    <span class="flex flex-1 justify-between">Indonesia </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="日本語"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1ef-1f1f5.svg">
-                                    <span class="flex flex-1 justify-between">日本語 </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="한국어"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1f0-1f1f7.svg">
-                                    <span class="flex flex-1 justify-between">한국어 </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="Português"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1f5-1f1f9.svg">
-                                    <span class="flex flex-1 justify-between">Português </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="Русский"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1f7-1f1fa.svg">
-                                    <span class="flex flex-1 justify-between">Русский </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="中文"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1e8-1f1f3.svg">
-                                    <span class="flex flex-1 justify-between">中文 </span></button>
-                            </li>
-                            <li>
-                                <button class="flex"><img loading="lazy" width="20" height="20" alt="繁體中文"
-                                                          src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/1f1f9-1f1fc.svg">
-                                    <span class="flex flex-1 justify-between">繁體中文 </span></button>
+                                    <span class="flex flex-1 justify-between">{{ $t(`languages.${lang.code}`) }} </span></button>
                             </li>
                         </ul>
                     </div>
@@ -254,6 +209,8 @@
 
 <script>
 
+import strings from "./assets/strings.json";
+
 export default {
     name: "App",
     components: {
@@ -299,48 +256,7 @@ export default {
             base_code: 'USD',
             rates: [],
             focused_currency: 0,
-            languages: [
-                {
-                    name: 'English',
-                    code: 'en',
-                },
-                {
-                    name: 'French',
-                    code: 'fr',
-                },
-                {
-                    name: 'German',
-                    code: 'de',
-                },
-                {
-                    name: 'Spanish',
-                    code: 'es',
-                },
-                {
-                    name: 'Italian',
-                    code: 'it',
-                },
-                {
-                    name: 'Japanese',
-                    code: 'ja',
-                },
-                {
-                    name: 'Korean',
-                    code: 'ko',
-                },
-                {
-                    name: 'Portuguese',
-                    code: 'pt',
-                },
-                {
-                    name: 'Russian',
-                    code: 'ru',
-                },
-                {
-                    name: 'Chinese',
-                    code: 'zh',
-                },
-            ],
+            languages: [],
             locale: '',
         }
     },
@@ -375,6 +291,10 @@ export default {
             this.locale = locale;
             this.$i18n.locale = this.locale;
             localStorage.setItem('locale', this.locale);
+        },
+
+        getLanguages() {
+            this.languages = strings.languages;
         },
 
         sortCurrencies() {
@@ -467,8 +387,9 @@ export default {
     },
     mounted() {
         this.getDefaultTheme();
-        this.getLocale();
         document.documentElement.setAttribute('data-theme', this.currentTheme);
+        this.getLocale();
+        this.getLanguages();
         this.updateData();
         this.getData();
     },
