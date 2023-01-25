41e3df4659778169006f3e58433206523124e5d5
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-25 14:51:00 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import { createI18n } from 'vue-i18n';
 import { createApp } from 'vue';
 
 const i18n = createI18n({
-    locale: sessionStorage.getItem('locale') || 'en',
+    locale: localStorage.getItem('locale') || 'en',
     messages: strings
 });
 
